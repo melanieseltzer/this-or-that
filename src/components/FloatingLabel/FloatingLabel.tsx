4501071c52aeab7461cb91448f4d152a6108b2ed
Melanie Seltzer <melleh11@gmail.com>
2019-02-18 11:59:27 -0800

    Better description for React.cloneElement (#11)

--- src/components/FloatingLabel/FloatingLabel.tsx
+++ src/components/FloatingLabel/FloatingLabel.tsx
@@ -2,20 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FloatingLabel = (props: FloatingLabel) => {
-  // Since in the context of FloatingLabel we want the label to automatically float,
-  // we can clone each child and pass a float prop set to true, which we can leverage
-  // in the <Label> styled component (positioning)
   const { children } = props;
+
+  // Using the React.Children.map utility, we can map over each child and assign the float
+  // prop to each one using React.cloneElement, which we can then leverage for styling in
+  // Input and Label's styled components (if float is true then apply the float styles).
+  // Since this is only called within the FloatingLabel component, the float prop won't be
+  // applied if using an Input or Label outside of that context. This saves us from having
+  // to pass the float prop down to each child manually.
   const childrenWithProps = React.Children.map(children, child =>
     React.cloneElement(child, { float: true })
   );
 
-  return <RelativeWrapper>{childrenWithProps}</RelativeWrapper>;
+  return <StyledFloatingLabel>{childrenWithProps}</StyledFloatingLabel>;
 };
 
 export default FloatingLabel;
 
-const RelativeWrapper = styled.div`
+const StyledFloatingLabel = styled.div`
   position: relative;
   flex-basis: 100%;
   @media (min-width: 768px) {
